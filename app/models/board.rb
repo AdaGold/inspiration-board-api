b2b18b4b5b055f54d42b6efa13a25128c9ee8f56
CheezItMan <chris@adadevelopersacademy.org>
2019-06-06 14:39:25 -0700

    change creation of new boards

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,59 @@
 class Board < ApplicationRecord
 
+  CARDS = [
+    {
+      text: '100'
+    },
+    {
+      text: 'BE EXCELLENT TO EACHOTHER'
+    },
+    {
+      text: 'BREATHE'
+    },
+    {
+      text: 'Look for the helpers'
+    },
+    {
+      text: "Growth isn't linear"
+    },
+    {
+      text: "This is just the beginning"
+    },
+    {
+      text: "DON'T ISOLATE"
+    },
+    {
+      text: "TAKE A NAP"
+    },
+    {
+      text: "Make time for exercise!"
+    },
+    {
+      text: "Be good to people for no reason"
+    },
+    {
+      text: "It's ok to be in it for the money!  $$$"
+    },
+    {
+      text: "you are enough"
+    },
+    {
+      text: "We believe in you!"
+    },
+    {
+      text: "You can do it!"
+    },
+    {
+      emoji: Card.valid_emojis.sample
+    },
+    {
+      emoji: Card.valid_emojis.sample
+    },
+    {
+      emoji: Card.valid_emojis.sample
+    }
+  ]
+
   has_many :cards, dependent: :destroy
 
   validates :name, presence: true, allow_blank: false
@@ -8,16 +62,12 @@ class Board < ApplicationRecord
   def self.create_new_board name
     board = Board.new name: name
 
-    template = Board.first
-
     Board.transaction do
-
-      template.cards.each do |card|
-        board.cards << (Card.create text: card.text, emoji: card.emoji)
+      cards = CARDS.each do |card_fields|
+        board.cards << Card.create(card_fields)
       end
 
       board.save
-
     end
 
     return board
